782225bc6923657c575d5f08f3f15ba5979678eb
Christian Wulff <doooozer@gmx.net>
2023-11-23 17:50:35 +0100

    Add window styles to avoid hanging

---
+++ Parsify.Core/Forms/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsify.Core.Forms
+{
+    internal static class Extensions
+    {
+        public static void UpdateWindowStyles( this Form form )
+        {
+            foreach ( Control control in form.Controls )
+                UpdateControlWindowStyles( control );
+        }
+
+        private static void UpdateControlWindowStyles( Control control )
+        {
+            NativeWindowApi.ModifyStyleEx( control.Handle, 0, NativeWindowApi.WS_EX_CONTROLPARENT );
+
+            foreach ( Control child in control.Controls )
+                UpdateControlWindowStyles( child );
+        }
+    }
+}
